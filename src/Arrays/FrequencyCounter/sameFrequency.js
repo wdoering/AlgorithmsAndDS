5fecb72315cd3b83b89bf704456c53b2042bd15b
Wagner Doering <wdoering@k-lagan.com>
2020-04-13 18:30:42 +0200

    implementado método que compara dois numeros e diz se eles tem a mesma quantidade de cada algarismos.

--- src/Arrays/FrequencyCounter/sameFrequency.js
+++ src/Arrays/FrequencyCounter/sameFrequency.js
@@ -6,5 +6,17 @@
  * Complexity: O(n)
  */
 module.exports = function sameFrequency(first, second) {
+  const strFirst = first.toString();
+  const strSecond = second.toString();
+  if (strFirst.length != strSecond.length) return false;
+  const primeiro = {};
+
+  for (let letter of strFirst) {
+    primeiro[letter] ? ++primeiro[letter] : (primeiro[letter] = 1);
+  }
+  for (const letter of strSecond) {
+    if (!primeiro[letter]) return false;
+    primeiro[letter]--;
+  }
   return true;
 };
