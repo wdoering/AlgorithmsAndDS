ca8c16d4ba44cf019fdae38a4ef4259e3133daf1
Wagner Doering <wdoering@k-lagan.com>
2020-04-14 12:00:25 +0200

    added next challenge

---
+++ src/Arrays/MultiplePointers/averagePair.js
@@ -0,0 +1,10 @@
+/**
+ *
+ * @param {any} numbers list of numbers
+ * @param {any} average value to be found dividing any 2 numbers in the first param
+ * @returns {boolean} returns weather at least the average of any 2 items in the array is equal to the second param
+ * Complexity: O(n)
+ */
+module.exports = function averagePair(numbers, average) {
+  //   return true;
+};
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-// import sameFrequency from "./Arrays/FrequencyCounter/sameFrequency";
-const sameFrequency = require("./Arrays/FrequencyCounter/sameFrequency");
 //Just a class to import and run whatever exercise I'm working on
+const sameFrequency = require("./Arrays/FrequencyCounter/sameFrequency");
 
-const result = sameFrequency(123, 321);
+const result = sameFrequency(123, 3213);
 console.log(result);
